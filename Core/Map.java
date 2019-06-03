5360cc5882a7120fd626085b3b600e04a30e9c50
Tam <ttnguyen2203@berkeley.edu>
2019-06-02 17:38:47 -0700

    overworld generation with noise

--- Core/Map.java
+++ Core/Map.java
@@ -1,24 +1,65 @@
 package byog.Core;
 
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+import byog.Core.MapGen.OpenSimplexNoise;
+import byog.Core.RandomUtils;
+
+import org.junit.Test;
+
 import java.util.HashMap;
+import java.util.Random;
+
 public class Map {
     /*
         Class that keeps track of one local map (overworld or dungeon)
      */
+    public String name;
     private int H; // might not need to store these
     private int W;
-    private String[][] map;
+    private TETile[][] map;
     private int[][] render_window;
     private int[] player;
-    private HashMap<String, TETile> tile_map;
+    private Random random = new Random(123456789l);
 
-
-    public Map(int H, int W) {
+    public Map(String name, int H, int W) {
+        this.name = name;
         this.H = H;
         this.W = W;
 
-         // handle world gen
+        double c = 0.17;
+        this.map = new TETile[H][W];
+        OpenSimplexNoise generator = new OpenSimplexNoise();
+        // generate noise
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                double val =  generator.eval((double)i * c, (double) j * c);
+                this.map[i][j] = terrain_pick(val);
+
+            }
+        }
+    }
+
+    public TETile[][] getMap() {
+        return this.map;
+    }
+
+
+    private TETile terrain_pick(double val) {
+        double normalized = (val + 1) * 5;
+        if (normalized < 3) {
+            return Tileset.WATER;
+        } else if (normalized < 3.5) {
+            return Tileset.SAND;
+        } else if (normalized < 6.3) {
+            return Tileset.GRASS;
+        } else if (normalized < 7.4) {
+            return Tileset.TREE;
+        } else if (normalized < 8) {
+            return Tileset.MOUNTAIN;
+        } else {
+            return Tileset.SNOWM;
+        }
 
     }
 
--- TileEngine/Tileset.java
+++ TileEngine/Tileset.java
@@ -6,9 +6,6 @@ import java.awt.Color;
  * Contains constant tile objects, to avoid having to remake the same tiles in different parts of
  * the code.
  *
- * You are free to (and encouraged to) create and add your own tiles to this file. This file will
- * be turned in with the rest of your code.
- *
  * Ex:
  *      world[x][y] = Tileset.FLOOR;
  *
@@ -23,16 +20,21 @@ public class Tileset {
     public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
             "floor");
     public static final TETile NOTHING = new TETile(' ', Color.black, Color.black, "nothing");
-    public static final TETile GRASS = new TETile('"', Color.green, Color.black, "grass");
-    public static final TETile WATER = new TETile('≈', Color.blue, Color.black, "water");
+    public static final TETile GRASS = new TETile('v', new Color(37, 122, 22), new Color(136, 192, 102), "grass");
+    public static final TETile GRASS1 = new TETile('"', new Color(187, 212, 175), new Color(120, 192, 113), "grass");
+
+    public static final TETile WATER = new TETile('≈', new Color(53, 73, 248), new Color(74, 207, 255), "water");
     public static final TETile FLOWER = new TETile('❀', Color.magenta, Color.pink, "flower");
     public static final TETile LOCKED_DOOR = new TETile('█', Color.orange, Color.black,
             "locked door");
     public static final TETile UNLOCKED_DOOR = new TETile('▢', Color.orange, Color.black,
-            "unlocked door");
-    public static final TETile SAND = new TETile('▒', Color.yellow, Color.black, "sand");
-    public static final TETile MOUNTAIN = new TETile('▲', Color.gray, Color.black, "mountain");
-    public static final TETile TREE = new TETile('♠', Color.green, Color.black, "tree");
+            "unlocked door"); // OLD SAND CHARACTER '▒'
+    public static final TETile SAND = new TETile('▒', new Color(219, 198, 0, 255), new Color(219, 198, 0), "sand");
+    public static final TETile MOUNTAIN = new TETile('▲', new Color(187, 212, 175), new Color(120, 192, 113), "mountain");
+    public static final TETile SNOWM = new TETile('▲', new Color(255, 253, 254), new Color(187, 212, 175), "mountain");
+
+
+    public static final TETile TREE = new TETile('♠', new Color(25, 110, 54), new Color(107, 156, 87), "tree");
 }
 
 
